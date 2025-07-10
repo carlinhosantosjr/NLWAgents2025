a2f5f6477f78144cf37d9cbb523617f7f4cf28d2
carlinhosantosjr <carlinhosantosjr@hotmail.com>
2025-07-10 17:34:24 -0300

    100% project

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -7,16 +7,19 @@ await reset(db, schema)
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
       },
+    },
+    questions: {
+      count: 20,
     },
   }
 })
 
 await sql.end()
 
-// biome-ignore lint/suspicious/noConsole: <explanation>
+// biome-ignore lint/suspicious/noConsole: only used in dev
 console.log('Database seeded')
